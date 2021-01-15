b2dad03a459c9f787350c6a4360247e569e9b0f7
liberaliscomputing <liberalis.computing@gmail.com>
2021-01-15 05:59:27 -0500

    :sparkles: add common area ui

--- pic-sure-hpds-ui/src/main/webapp/picsureui/overrides/outputPanel.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/overrides/outputPanel.js
@@ -1,4 +1,4 @@
-define(["jquery", "handlebars", "backbone"], function($, HBS, BB){
+define(["jquery", "handlebars", "backbone", "picSure/resourceMeta", "picSure/queryCache"], function ($, HBS, BB, resourceMeta, queryCache) {
 	return {
 		/*
 		 * This should be a function that returns the name of a Handlebars partial
@@ -27,12 +27,46 @@ define(["jquery", "handlebars", "backbone"], function($, HBS, BB){
 		 * and dispatches it to the resources you choose, and handles registering
 		 * callbacks for the responses and error handling.
 		 */
-		update: undefined,
+		update: function (incomingQuery) {
+			this.model.set("totalPatients", 0);
+			
+			this.model.spinAll();
+			this.render();
+
+			_.each(resourceMeta, function (picsureInstance) {
+				// make a safe deep copy of the incoming query so we don't modify it
+				var query = JSON.parse(JSON.stringify(incomingQuery));
+
+				var dataCallback = function (result) {
+					if (result === undefined || result.status === "ERROR") {
+						this.model.get("resources")[picsureInstance.id].patientCount = 0;
+					} else {
+						var count = parseInt(result.data[0][0].patient_set_counts);
+						this.model.get("resources")[picsureInstance.id].queryRan = true;
+						this.model.get("resources")[picsureInstance.id].patientCount = count;
+						this.model.set("totalPatients", this.model.get("totalPatients") + count);
+					}
+
+					this.model.get("resources")[picsureInstance.id].spinning = false;
+					if (_.every(this.model.get("resources"), function (resource) { return resource.spinning === false; })) {
+						this.model.set("spinning", false);
+						this.model.set("queryRan", true);
+					}
+					this.render();
+				}.bind(this);
+
+				queryCache.submitQuery(
+					picsureInstance,
+					query,
+					picsureInstance.id,
+					dataCallback
+				);
+			}.bind(this));
+		},
 		/*
 		 * If you want to show your customized error message, please override this
 		 */
 		outputErrorMessage: undefined,
-		
 		/*
 		 * A function to make any necessary updates to the query before submitting
 		 */
